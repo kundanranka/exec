9539678cf81e32bfaac0cd608ac9d037bd6ee46e
kundanranka <52109355+kundanranka@users.noreply.github.com>
2019-09-24 20:21:19 +0530

    exec changes

--- exec_demo.c
+++ exec_demo.c
@@ -4,6 +4,6 @@
 
 int main(int argc,char*argv[])
 {
-	printf("I am %s and my PID is : %d\n",argv[0],getpid());
+	printf("I am %s and my PID is : %d and my PPID is: %d\n",argv[0],getpid(),getppid());
 	return 0;
 }
