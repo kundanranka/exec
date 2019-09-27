a6a967b4010c0b40b4c08b3c3431571ea2f7bbc1
kundanranka <52109355+kundanranka@users.noreply.github.com>
2019-09-27 07:01:41 +0530

    exec

--- exec_call.c
+++ exec_call.c
@@ -4,7 +4,7 @@
 
 int main(int argc,char*argv[])
 {
-	printf("I am %s and my PID is : %d and my PPID is:%d\n",argv[0],getpid(),getppid());
+	printf("I am %s, my PID is : %d and my PPID is:%d\n",argv[0],getpid(),getppid());
 	execlp("./exec_demo","./exec_demo",NULL);
 	return 0;
 }
--- exec_demo.c
+++ exec_demo.c
@@ -4,6 +4,6 @@
 
 int main(int argc,char*argv[])
 {
-	printf("I am %s and my PID is : %d and my PPID is: %d\n",argv[0],getpid(),getppid());
+	printf("I am %s ,my PID is : %d and my PPID is: %d\n",argv[0],getpid(),getppid());
 	return 0;
 }
